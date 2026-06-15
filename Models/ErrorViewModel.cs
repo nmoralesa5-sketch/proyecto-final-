2b1209ab4124eaa59ccacd740725ab18ff45f9cf
NESTOR MIGUEL MORALES ALVAREZ <nmoralesa5@miumg.edu.gt>
2026-06-14 18:09:23 -0600

    Add files via upload
    Proyecto Final MVC Docker Render

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nestor_miguel_morales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
